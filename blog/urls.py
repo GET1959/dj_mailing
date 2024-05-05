81f5c25c16de095201404f6af918b3a0f2935f57
gennady <topchiev@mail.ru>
2024-05-05 08:42:15 +0300

    modified blog/urls.py article_list.html

--- blog/urls.py
+++ blog/urls.py
@@ -13,8 +13,8 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path("create/", never_cache(ArticleCreateView.as_view()), name="create"),
-    path("", cache_page(60)(ArticleListView.as_view()), name="list"),
-    path("view/<int:pk>/", ArticleDetailView.as_view(), name="view"),
+    path("", ArticleListView.as_view(), name="list"),
+    path("view/<int:pk>/", cache_page(60)(ArticleDetailView.as_view()), name="view"),
     path("delete/<int:pk>/", ArticleDeleteView.as_view(), name="delete"),
     path('activity/<int:pk>/', activity_trigger, name='activity_trigger'),
 ]
