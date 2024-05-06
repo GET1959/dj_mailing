3c37cfffa72c5dd2cd6c513ff344514ea211c8d4
gennady <topchiev@mail.ru>
2024-05-06 18:38:14 +0300

    modified settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,8 +13,6 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-from config.conf import get_pw, get_mail_pw
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
