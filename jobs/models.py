f9a4825f43397e5599c64cf248acb661a67e9fb6
gennady <topchiev@mail.ru>
2024-05-14 19:22:05 +0300

    modified models.py

--- jobs/models.py
+++ jobs/models.py
@@ -8,15 +8,23 @@ NULLABLE = {"blank": True, "null": True}
 
 
 class Mailing(models.Model):
+    FREQ_CHOICES = [
+        ("10 секунд", "10 секунд"),
+        ("минута", "минута"),
+        ("день", "день"),
+        ("неделя", "неделя"),
+        ("месяц", "месяц"),
+    ]
+
     title = models.CharField(max_length=150, verbose_name="рассылка")
     recipients = models.ManyToManyField(Client, verbose_name="Получатели")
     message = models.ForeignKey(Message, default=0, on_delete=models.CASCADE, verbose_name="сообщение")
     start_time = models.DateTimeField(verbose_name="старт рассылки")
     stop_time = models.DateTimeField(verbose_name="окончание рассылки")
     next_sending = models.DateTimeField(verbose_name="следующая отправка")
-    frequency = models.CharField(max_length=50, verbose_name="периодичность рассылки")
+    frequency = models.CharField(max_length=50, choices=FREQ_CHOICES, verbose_name="периодичность рассылки")
     status = models.CharField(max_length=150, default="created", verbose_name="статус рассылки")
-    is_active = models.BooleanField(**NULLABLE, verbose_name="активна")
+    is_active = models.BooleanField(**NULLABLE, default=True, verbose_name="активна")
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='владелец')
 
     def __str__(self):
