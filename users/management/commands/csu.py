4863c4c673d77832c80271ec83071d5a76a9896e
gennady <topchiev@mail.ru>
2024-10-07 21:46:41 +0300

    обновлен README.md

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,18 +1,26 @@
+import os
+
 from django.core.management import BaseCommand
+from dotenv import load_dotenv
 
 from users.models import User
+
+load_dotenv()
+
+SU_EMAIL = os.getenv("SU_EMAIL")
+SU_PASSWORD = os.getenv("SU_PASSWORD")
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         user = User.objects.create(
-            email="admin@sky.pro",
-            first_name="admin",
-            last_name="SkyPro",
+            email=SU_EMAIL,
+            first_name="admin_first_name",
+            last_name="admin_last_name",
             is_staff=True,
             is_superuser=True,
         )
 
-        user.set_password("1234")
+        user.set_password(SU_PASSWORD)
         user.save()
